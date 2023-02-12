45cff42f9b793fc272f5c8e089edd40e0fc9d744
Juan Villarreal <juan@juan.local>
2023-02-12 11:27:20 -0300

    Added postData() for console logging

--- portfolio/src/components/Create.js
+++ portfolio/src/components/Create.js
@@ -1,4 +1,4 @@
-import { Button, Checkbox, Form } from "semantic-ui-react";
+import { Button, Form } from "semantic-ui-react";
 import React, { useState } from "react";
 
 const options = [
@@ -6,19 +6,40 @@ const options = [
   { key: "s", text: "Sold", value: "sold" },
 ];
 
-const Create = () => (
-  <Form className="create-form">
-    <Form.Field>
-      <label>NFT</label>
-      <input placeholder="NFT" />
-    </Form.Field>
-    <Form.Field>
-      <label>Price</label>
-      <input type="number" step="0.01" placeholder="Price" />
-    </Form.Field>
-    <Form.Select fluid label="Status" options={options} placeholder="Status" />
-    <Button type="submit">Submit</Button>
-  </Form>
-);
-
-export default Create;
+export default function Create() {
+  const [nft, setNFT] = useState("");
+  const [price, setPrice] = useState("");
+  const [nftstatus, setStatus] = useState("");
+  const postData = () => {
+    console.log(nft);
+    console.log(price);
+    console.log(nftstatus);
+  };
+  return (
+    <Form className="create-form">
+      <Form.Field>
+        <label>NFT</label>
+        <input placeholder="NFT" onChange={(e) => setNFT(e.target.value)} />
+      </Form.Field>
+      <Form.Field>
+        <label>Price</label>
+        <input
+          type="number"
+          step="0.01"
+          placeholder="Price"
+          onChange={(e) => setPrice(e.target.value)}
+        />
+      </Form.Field>
+      <Form.Select
+        fluid
+        label="Status"
+        options={options}
+        placeholder="Status"
+        onChange={(e) => setStatus(e.target.value)}
+      />
+      <Button onClick={postData} type="submit">
+        Submit
+      </Button>
+    </Form>
+  );
+}
