992fab854e3eadf660b8e02357fca4d846c688e5
ju4ncito <villarrealjm2@gmail.com>
2023-02-20 10:47:27 -0300

    Added alert to prevent empty fields, added alert for tx created and timeout for tx created message

--- portfolio/src/components/Create.js
+++ portfolio/src/components/Create.js
@@ -10,6 +10,7 @@ export default function Create() {
   const [nft, setNFT] = useState("");
   const [price, setPrice] = useState("");
   const [nftstatus, setStatus] = useState("");
+  const [transactionCreated, setTransactionCreated] = useState(false);
 
   const postData = async () => {
     console.log(nft);
@@ -17,56 +18,71 @@ export default function Create() {
     console.log(nftstatus.toLowerCase());
 
     await postTx(nft, price, nftstatus);
+    setTransactionCreated(true);
+    setTimeout(() => {
+      setTransactionCreated(false);
+    }, 3000);
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    if (!nft || !price || !nftstatus) {
+      alert("Please fill in all the fields.");
+      return;
+    }
     postData();
   };
 
   return (
-    <form className="create-form" onSubmit={handleSubmit}>
-      <div className="form-field">
-        <label htmlFor="nft">NFT</label>
-        <input
-          id="nft"
-          name="nft"
-          type="text"
-          placeholder="NFT"
-          value={nft}
-          onChange={(e) => setNFT(e.target.value)}
-        />
-      </div>
-      <div className="form-field">
-        <label htmlFor="price">Price</label>
-        <input
-          id="price"
-          name="price"
-          type="number"
-          step="0.01"
-          placeholder="Price"
-          value={price}
-          onChange={(e) => setPrice(e.target.value)}
-        />
-      </div>
-      <div className="form-field">
-        <label htmlFor="status">Status</label>
-        <select
-          id="status"
-          name="status"
-          value={nftstatus}
-          onChange={(e) => setStatus(e.target.value)}
-        >
-          <option value="">Select status</option>
-          {options.map(({ key, text, value }) => (
-            <option key={key} value={value}>
-              {text}
-            </option>
-          ))}
-        </select>
-      </div>
+    <div>
+      <form className="create-form" onSubmit={handleSubmit}>
+        <div className="form-field">
+          <label htmlFor="nft">NFT</label>
+          <input
+            id="nft"
+            name="nft"
+            type="text"
+            placeholder="NFT"
+            value={nft}
+            onChange={(e) => setNFT(e.target.value)}
+          />
+        </div>
+        <div className="form-field">
+          <label htmlFor="price">Price</label>
+          <input
+            id="price"
+            name="price"
+            type="number"
+            step="0.01"
+            placeholder="Price"
+            value={price}
+            onChange={(e) => setPrice(e.target.value)}
+          />
+        </div>
+        <div className="form-field">
+          <label htmlFor="status">Status</label>
+          <select
+            id="status"
+            name="status"
+            value={nftstatus}
+            onChange={(e) => setStatus(e.target.value)}
+          >
+            <option value="">Select status</option>
+            {options.map(({ key, text, value }) => (
+              <option key={key} value={value}>
+                {text}
+              </option>
+            ))}
+          </select>
+        </div>
 
-      <button type="submit">Submit</button>
-    </form>
+        <button type="submit">Submit</button>
+      </form>
+
+      <div>
+        {!transactionCreated && <p>.</p>}
+        {transactionCreated && <p>NFT added correctly</p>}
+      </div>
+    </div>
   );
 }
